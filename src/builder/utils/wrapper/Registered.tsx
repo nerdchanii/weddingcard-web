01c3a54acda92bc86803dd443186714ce2f1d156
nerdchanii <nerdchanii@gmail.com>
2022-10-07 16:04:48 +0900

    Remove: 당장 불필요한 부분 일단 삭제

--- src/builder/utils/wrapper/Registered.tsx
+++ src/builder/utils/wrapper/Registered.tsx
@@ -1,17 +1,15 @@
-import React, { useRef } from "react";
+import React from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Registered = ({ children, parent }: Props) => {
-  const [currentPosition, setCurrentPosition] = React.useState({ x: 0, y: 20 });
   
   return (
     <div
       draggable={true}
-      className="border-2 border-sky-500 inline-block absolute"
-      style={{ left: currentPosition.x, top: currentPosition.y }}
+      className="border-2 border-sky-500 inline-block"
     >
       {children}
     </div>
--- src/components/builder/BuilderContainer.tsx
+++ src/components/builder/BuilderContainer.tsx
@@ -1,23 +1,15 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import BuilderDropArea from "./BuilderDropArea";
-import BuilderOptions from "./BuilderOptions";
-import FormFacktor from "./FormFacktor";
+// import BuilderOptions from "./BuilderOptions";
+// import FormFacktor from "./FormFacktor";
 
 type Props = {};
 
 const BuilderContainer = (props: Props) => {
-  const [device,setDevice] = useState<'iphone'|'android'>('iphone');
-  let x,y = 0;
-  
 
   return (
-    <div className="min-w-1/2 h-screen bg-red-400 md:w-1/2 md:mb-96 flex flex-col justify-center border-2 p-4 z-20">
-      <div className="bg-white z-10">
-        <BuilderOptions setDevice={setDevice} /> 
-      </div>
-      <FormFacktor device={device}>
-        <BuilderDropArea />
-      </FormFacktor>
+    <div className="min-w-1/2 h-screen bg-stone-300 md:w-1/2 md:mb-96 flex flex-col justify-center border-2 p-4 z-20">
+      <BuilderDropArea />
     </div>
   );
 };
